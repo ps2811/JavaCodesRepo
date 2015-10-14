3303b0383190f95886a6e60131c78de4b8c8b709
Pooja <mail.pooja2811@gmail.com>
2015-10-13 22:41:53 -0400

    Inital Checkin for GitLearning

---
+++ LearningGit/src/myJavaCodes/Main.java
@@ -0,0 +1,15 @@
+package myJavaCodes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SalesTool data = new SalesTool();
+		displayGreetings();
+		data.display();
+	}
+	
+	public static void displayGreetings() {
+		System.out.println("Hello Happy People :)");
+		System.out.println("This app shows Sales data");
+	}
+}
---
+++ LearningGit/src/myJavaCodes/SalesTool.java
@@ -0,0 +1,10 @@
+package myJavaCodes;
+
+public class SalesTool {
+	int data[] = {0,4,42};
+	
+	public void display() {
+	   System.out.println("Inside SalesTool class");	
+	}
+
+}

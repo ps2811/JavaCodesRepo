0879fff1b3e4918fa39563fb188830ef4adcf6bb
Pooja <mail.pooja2811@gmail.com>
2015-10-13 23:18:57 -0400

    -Added a second display to SalesTool class.

--- LearningGit/src/myJavaCodes/SalesTool.java
+++ LearningGit/src/myJavaCodes/SalesTool.java
@@ -4,7 +4,9 @@ public class SalesTool {
 	int data[] = {0,4,42};
 	
 	public void display() {
-	   System.out.println("Inside SalesTool class");	
+	   System.out.println("Inside SalesTool class");
+	   
+	   System.out.println("Insode SalesTool 2");
 	}
 
 }
